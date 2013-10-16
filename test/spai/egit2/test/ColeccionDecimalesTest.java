652716fdf9a3b71a3e5d7a4c0969de4371fb2e3a
mrrm <manuel.rodriguez@pucp.pe>
2013-10-16 17:39:31 +0200

    Test para ubicar el menor de una colección de decimales

--- test/spai/egit2/test/ColeccionDecimalesTest.java
+++ test/spai/egit2/test/ColeccionDecimalesTest.java
@@ -8,22 +8,26 @@ import org.junit.Test;
 import spai.egit2.ColeccionDecimales;
 
 public class ColeccionDecimalesTest {
-    private ColeccionDecimales numeros; 
+	private ColeccionDecimales numeros;
 
-    @Before
-    public void initObjects() {
-        double[] coleccion = {1.2, 3.4, 1.2};
-        this.numeros = new ColeccionDecimales(coleccion);
-    }
+	@Before
+	public void initObjects() {
+		double[] coleccion = { 1.2, 3.4, 1.2 };
+		this.numeros = new ColeccionDecimales(coleccion);
+	}
 
-    @Test
-    public void testSuma() {
-        assertEquals(5.8, this.numeros.suma(), 1e-10);
-    }
+	@Test
+	public void testSuma() {
+		assertEquals(5.8, this.numeros.suma(), 1e-10);
+	}
 
-    @Test
-    public void testMayor() {
-        assertEquals(3.4, this.numeros.mayor(), 1e-10);
-    }
+	@Test
+	public void testMayor() {
+		assertEquals(3.4, this.numeros.mayor(), 1e-10);
+	}
 
+	@Test
+	public void testMenor() {
+		assertEquals(1.2, this.numeros.menor(), 1e-10);
+	}
 }
