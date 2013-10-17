699b3140a5c27f494c0d20003a26310c228139d8
mrrm <manuel.rodriguez@pucp.pe>
2013-10-17 13:30:28 +0200

    implementado metodo mayuscula de nombre

--- src/spai/egit2/Persona.java
+++ src/spai/egit2/Persona.java
@@ -1,46 +1,47 @@
 package spai.egit2;
 
 public class Persona {
-    private String nombre;
+	private String nombre;
 
-    private String apellido;
+	private String apellido;
 
-    private String format(String cadena) {
-        cadena = cadena.trim();
-        return cadena.substring(0, 1).toUpperCase() + cadena.substring(1).toLowerCase();
-    }
+	private String format(String cadena) {
+		cadena = cadena.trim();
+		return cadena.substring(0, 1).toUpperCase()
+				+ cadena.substring(1).toLowerCase();
+	}
 
-    public Persona(String nombre, String apellido) {
-        this.setNombre(nombre);
-        this.setApellido(apellido);
-    }
+	public Persona(String nombre, String apellido) {
+		this.setNombre(nombre);
+		this.setApellido(apellido);
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = this.format(nombre);
-    }
+	public void setNombre(String nombre) {
+		this.nombre = this.format(nombre);
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public String getApellido() {
+		return apellido;
+	}
 
-    public void setApellido(String apellido) {
-        this.apellido = this.format(apellido);
-    }
+	public void setApellido(String apellido) {
+		this.apellido = this.format(apellido);
+	}
 
-    public String nombreCompleto() {
-        return this.nombre + " " + this.apellido;
-    }
+	public String nombreCompleto() {
+		return this.nombre + " " + this.apellido;
+	}
 
-    public String inicial() {
-        return this.nombre.substring(0, 1) + ".";
-    }
-    
-    public String nombreMayusculas(){
-    	return null;
-    }
+	public String inicial() {
+		return this.nombre.substring(0, 1) + ".";
+	}
+
+	public String nombreMayusculas() {
+		return nombre.toUpperCase();
+	}
 
 }
